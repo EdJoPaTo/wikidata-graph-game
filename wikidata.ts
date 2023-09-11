5255e5e113498a60d5e95174b3cfb65f7bf7ac03
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-09-11 11:51:39 +0200

    feat(wikidata): add search API

--- wikidata.ts
+++ wikidata.ts
@@ -5,10 +5,12 @@ import {
 	type Item,
 	type ItemId,
 	type PropertyClaims,
+	type SearchResponse,
 	type StringSnakValue,
 	WBK,
 	type WikibaseEntityIdSnakValue,
 } from "https://esm.sh/wikibase-sdk@9.2.2";
+import { cache, getCached } from "./store.ts";
 
 const SUBCLASS_OF = "P279";
 const PARENT_TAXON = "P171";
@@ -22,6 +24,44 @@ export const wdk = WBK({
 	instance: "https://www.wikidata.org",
 	sparqlEndpoint: "https://query.wikidata.org/sparql",
 });
+
+interface RelevantSearchResult {
+	id: ItemId;
+	taxonName?: string;
+	label: string;
+	description?: string;
+}
+
+export async function search(
+	search: string,
+	language: string,
+): Promise<RelevantSearchResult[]> {
+	const url = wdk.searchEntities({
+		search: `${search}`,
+		language,
+		type: "item",
+	});
+	const response = await fetch(url, { headers });
+	const { search: results } = await response.json() as SearchResponse;
+
+	const itemIds = results.map((o) => o.id).filter(isItemId);
+	await cache(itemIds);
+
+	const relevant = results.map((o): RelevantSearchResult | undefined => {
+		const id = o.id;
+		if (!isItemId(id)) return undefined;
+		const item = getCached(id)!;
+		if (getItemParents(item).length === 0) return undefined;
+		return {
+			id,
+			taxonName: getTaxonName(item),
+			label: o.label,
+			description: o.description,
+		};
+	}).filter((o): o is RelevantSearchResult => o !== undefined);
+
+	return relevant;
+}
 
 interface GetEntitiesResponse {
 	entities: Record<EntityId, Entity>;
