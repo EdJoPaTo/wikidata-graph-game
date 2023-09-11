c2aa9442e2514f4efcab2eaaba9e13f31ffb3427
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-09-11 10:28:31 +0200

    feat: add more GUESSES

--- index.ts
+++ index.ts
@@ -6,23 +6,37 @@ import * as store from "./store.ts";
 
 const TARGET = "Q83483"; // sea urchin
 const GUESSES: readonly ItemId[] = [
-	"Q123141", // Goldfish
-	"Q127470", // Seegurke
-	"Q25349", // Starfish
-	"Q25349", // Starfish
-	"Q25407", // Bumble bee
-	"Q2699803", // Sea gull
-	"Q53663", // Salamander
+	// "Q10321095", // Otter which doesnt work well
 	// "Q11019", // machine
 	// "Q11687", // Springspinne
+	"Q123141", // Goldfish
+	"Q127470", // Seegurke
 	// "Q13235160", // manufacturer
 	// "Q140", // lion
 	// "Q146", // house cat
+	// "Q15083", // Giraffe
+	// "Q15504328", // Salmoninae
+	// "Q15978631", // human
+	// "Q192272", // Mammut
+	// "Q200184", // Otter
+	// "Q204175", // Hamster
+	// "Q2258881", // Trout
 	// "Q25265", // Felidae (from house cat)
+	"Q25349", // Starfish
+	"Q25407", // Bumble bee
+	// "Q25900", // Familiy of Rabbit
+	"Q2699803", // Sea gull
+	"Q53663", // Salamander
+	// "Q59166", // Quastenflossler
 	// "Q611843", // Octopus
 	// "Q6120", // Hedgehog
 	// "Q783794", // company
+	// "Q79803", // Cuy (Guinea pig)
 	// "Q830", // cattle
+	// "Q83310", // Hausmaus
+	"Q83483", // Sea urchin
+	// "Q9482", // squirrel
+	// "Q9490", // Gleithörnchen
 ];
 
 async function cacheWithParents(ids: ItemId[]): Promise<void> {
