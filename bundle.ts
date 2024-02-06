60b9472e81a8c3a2ab7cef4a9f3c4815c2f55527
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-02-06 04:08:45 +0100

    perf(bundle): minify
    Its a mess anyway but it has source maps. Minify the mess.

--- bundle.ts
+++ bundle.ts
@@ -1,6 +1,7 @@
 import { bundle } from "https://deno.land/x/emit@0.34.0/mod.ts";
 
 const { code } = await bundle("web.ts", {
+	minify: true,
 	compilerOptions: {
 		inlineSourceMap: true,
 		inlineSources: true,
