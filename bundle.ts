12d8fc3458f33ee8f471761f75c8f70d92b9abe0
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-02-06 04:24:47 +0100

    build(bundle): write map file when generated
    its not mentioned when not inlined so keep inlining it for now

--- bundle.ts
+++ bundle.ts
@@ -1,6 +1,6 @@
 import { bundle } from "https://deno.land/x/emit@0.34.0/mod.ts";
 
-const { code } = await bundle("web.ts", {
+const { code, map } = await bundle("web.ts", {
 	minify: true,
 	compilerOptions: {
 		inlineSourceMap: true,
@@ -8,3 +8,8 @@ const { code } = await bundle("web.ts", {
 	},
 });
 await Deno.writeTextFile("public/logic.js", code);
+if (map) {
+	await Deno.writeTextFile("public/logic.js.map", map);
+} else {
+	await Deno.remove("public/logic.js.map").catch(() => {});
+}
